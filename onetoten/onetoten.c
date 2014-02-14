d3633275554fa4460ebe87b533783689446175d6
mnxoid <sergolwow@gmail.com>
2014-02-14 23:17:46 +0200

    const int N=10 included for better code style

--- onetoten/onetoten.c
+++ onetoten/onetoten.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+const int N=10;
 int main()
 {
 	int i,s=0;
-	for (i=0;i<10;i++) s+=i;
-	printf("The sum of 10 first integers is: %i\n",s);
+	for (i=0;i<N;i++) s+=i;
+	printf("The sum of %i first integers is: %i\n",N,s);
 	char nothing=getchar();
 	return 0;
 }
