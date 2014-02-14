23612c162eb85125b8e6d410f8d260d68a024de9
mnxoid <sergolwow@gmail.com>
2014-02-14 23:22:33 +0200

    File info added

--- onetoten/onetoten.c
+++ onetoten/onetoten.c
@@ -1,3 +1,17 @@
+/** 
+ * @file	onetoten.c
+ * @brief	C source of the app that displays the sum of first 10 integers
+ *
+ * 
+
+ * Copyright 2014 by mnxoid,
+ * 
+ * This software is the confidential and proprietary information
+ * of mnxoid ("Confidential Information").  You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with mnxoid.
+ */
 #include <stdio.h>
 const int N=10;
 int main()
