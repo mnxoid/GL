d01ebd0213697e27e3027ba5c7390cba8826463a
mnxoid <sergolwow@gmail.com>
2014-02-15 13:48:17 +0200

    code added, application complete

--- binop/binop.c
+++ binop/binop.c
@@ -1,7 +1,7 @@
 /** 
  * @file	binop.c
- * @brief	C source of the app that ...
- *
+ * @brief	C source of the app that performs basic bitwise operations
+ *			and numeral system conversions 
  * 
  * Copyright 2014 by mnxoid,
  * 
@@ -12,9 +12,58 @@
  * you entered into with mnxoid.
  **/
 #include <stdio.h>
+void BinOut(int n)
+ {
+ 	int k=0;
+	while (k<8*sizeof(int))
+	 {
+		if (n & (1 << 8*sizeof(int)-1))
+			printf("1");
+		else
+			printf("0");
+		n <<= 1;
+		k++;
+	 }
+ }
+void AllOut(int n)
+ {
+ 	printf("\t\t%i\t",n);
+ 	BinOut(n);
+ 	printf("\t0x%-9x\t0%o\n",n,n);
+ }
 int main()
 {
-	//put da code here
+	int a,b;
+	printf("Input two numbers:\nA=");
+	scanf("%i",&a);
+	printf("B=");
+	scanf("%i",&b);
+	printf("\t\tDec\tBin\t\t\t\t\tHex\t\tOct\n");
+	printf("A:\n");
+	AllOut(a);
+	printf("B:\n");
+	AllOut(b);
+	printf("NOT A:\n");
+	AllOut(~a);
+	printf("NOT B:\n");
+	AllOut(~b);
+	printf("AND A,B:\n");
+	AllOut(a&b);
+	printf("OR A,B:\n");
+	AllOut(a|b);
+	printf("XOR A,B:\n");
+	AllOut(a^b);
+	int s;
+	printf("Enter shift value:\nS=");
+	scanf("%i",&s);
+	printf("SAL A,%i:\n",s);
+	AllOut(a<<s);
+	printf("SAL B,%i:\n",s);
+	AllOut(b<<s);
+	printf("SAR A,%i:\n",s);
+	AllOut(a>>s);
+	printf("SAR A,%i:\n",s);
+	AllOut(b>>s);
 	char nothing=getchar();
 	return 0;
 }
--- binop/binop.cpp
+++ binop/binop.cpp
@@ -1,7 +1,7 @@
 /** 
  * @file	binop.cpp
- * @brief	C++ source of the app that ...
- *
+ * @brief	C++ source of the app that performs basic bitwise operations
+ *			and numeral system conversions 
  * 
  * Copyright 2014 by mnxoid,
  * 
@@ -14,9 +14,58 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
+void BinOut(int n)
+ {
+ 	int k=0;
+	while (k<8*sizeof(int))
+	 {
+		if (n & (1 << 8*sizeof(int)-1))
+			printf("1");
+		else
+			printf("0");
+		n <<= 1;
+		k++;
+	 }
+ }
+void AllOut(int n)
+ {
+ 	cout << "\t\t" << n << "\t";
+ 	BinOut(n);
+ 	printf("\t0x%-9x\t0%o\n",n,n);
+ }
 int main()
 {
-	//put da code here
+	int a,b;
+	cout << "Input two numbers:\nA=";
+	cin >> a;
+	cout << "B=";
+	cin >> b;
+	cout << "\t\tDec\tBin\t\t\t\t\tHex\t\tOct\n";
+	cout << "A:" << endl;
+	AllOut(a);
+	cout << "B:" << endl;
+	AllOut(b);
+	cout << "NOT A:" << endl;
+	AllOut(~a);
+	cout << "NOT B:" << endl;
+	AllOut(~b);
+	cout << "AND A,B:" << endl;
+	AllOut(a&b);
+	cout << "OR A,B:" << endl;
+	AllOut(a|b);
+	cout << "XOR A,B:" << endl;
+	AllOut(a^b);
+	int s;
+	cout << "Enter shift value:\nS=";
+	cin >> s;
+	cout << "SAL A," << s << ":" << endl;
+	AllOut(a<<s);
+	cout << "SAL B," << s << ":" << endl;
+	AllOut(b<<s);
+	cout << "SAR A," << s << ":" << endl;
+	AllOut(a>>s);
+	cout << "SAR B," << s << ":" << endl;
+	AllOut(b>>s);
 	char nothing=getchar();
 	return 0;
 }
