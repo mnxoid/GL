72cffd81e2e7079dcf0d47cf7c344a80f9ad5d21
mnxoid <sergolwow@gmail.com>
2014-02-15 10:51:53 +0200

    Initial commit

---
+++ binop/binop.c
@@ -0,0 +1,20 @@
+/** 
+ * @file	binop.c
+ * @brief	C source of the app that ...
+ *
+ * 
+ * Copyright 2014 by mnxoid,
+ * 
+ * This software is the confidential and proprietary information
+ * of mnxoid ("Confidential Information").  You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with mnxoid.
+ **/
+#include <stdio.h>
+int main()
+{
+	//put da code here
+	char nothing=getchar();
+	return 0;
+}
---
+++ binop/binop.cpp
@@ -0,0 +1,22 @@
+/** 
+ * @file	binop.cpp
+ * @brief	C++ source of the app that ...
+ *
+ * 
+ * Copyright 2014 by mnxoid,
+ * 
+ * This software is the confidential and proprietary information
+ * of mnxoid ("Confidential Information").  You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with mnxoid.
+ **/
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	//put da code here
+	char nothing=getchar();
+	return 0;
+}
