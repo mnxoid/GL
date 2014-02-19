5a592b674e5ad78fc661d5911547451d3fb88127
mnxoid <sergolwow@gmail.com>
2014-02-19 13:17:03 +0200

    Initial commit

---
+++ pract_task/sport_2/sport_2.c
@@ -0,0 +1,21 @@
+/** 
+ * @file	sport_2.c
+ * @brief	C source of the app that gets data about sportsmen
+ *			from stdin and outputs them grouped by sport
+ *
+ * 
+ * Copyright 2014 by mnxoid,
+ * 
+ * This software is the confidential and proprietary information
+ * of mnxoid ("Confidential Information").  You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with mnxoid.
+ **/
+#include <stdio.h>
+int main()
+{
+	//put da code here
+	char nothing=getchar();
+	return 0;
+}
